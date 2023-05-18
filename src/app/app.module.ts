1bf74d76181c9e11e3044a189dbc1bfb7efc1929
ArnauLeinster <arnau.leinster@gmail.com>
2023-05-18 17:40:59 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileSettingsComponent } from './profile-settings/profile-settings.component';
+import { HomeComponent } from './home/home.component';
+import { BookComponent } from './books/books.component';
+import { BookViewComponent } from './book-view/book-view.component';
+import { AddBookComponent } from './add-book/add-book.component';
+import { UserBooksComponent } from './user-books/user-books.component';
+import { EditBooksComponent } from './edit-books/edit-books.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileSettingsComponent,
+    HomeComponent,
+    BookComponent,
+    BookViewComponent,
+    AddBookComponent,
+    UserBooksComponent,
+    EditBooksComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
